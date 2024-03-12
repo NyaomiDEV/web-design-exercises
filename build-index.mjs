fc6781efca6483a9b5705ffde765703602e7ce4a
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-03-12 15:31:34 +0100

    add build-index

---
+++ build-index.mjs
@@ -0,0 +1,10 @@
+#!/bin/env node
+
+import { readFile, writeFile, readdir } from "node:fs/promises"
+
+const dirs = (await readdir(".", { withFileTypes: true }))
+	.filter(x => x.isDirectory() && !x.name.startsWith("."))
+	.map(x => x.name);
+
+const html = (await readFile("index.template.html", "utf-8")).replace("{exercises}", dirs.map(x => `<li><a href="${x}">${x}</a></li>`).join("\n"));
+await writeFile("index.html", html);
