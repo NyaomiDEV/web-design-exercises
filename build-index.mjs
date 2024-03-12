95e4faee1d3985887e5f3dfa8b788155c8b3956b
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-03-12 15:41:59 +0100

    autotab

--- build-index.mjs
+++ build-index.mjs
@@ -6,5 +6,5 @@ const dirs = (await readdir(".", { withFileTypes: true }))
 	.filter(x => x.isDirectory() && !x.name.startsWith("."))
 	.map(x => x.name);
 
-const html = (await readFile("index.template.html", "utf-8")).replace("{exercises}", dirs.map(x => `<li><a href="${x}">${x}</a></li>`).join("\n"));
+const html = (await readFile("index.template.html", "utf-8")).replace("{exercises}", dirs.map(x => `\t\t\t<li><a href="${x}">${x}</a></li>`).join("\n"));
 await writeFile("index.html", html);
