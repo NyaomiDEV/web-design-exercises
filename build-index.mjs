c4e270bbd94f9bad7646f758a7bac96ed7f1c00f
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-03-19 14:24:16 +0100

    fix stuff

--- build-index.mjs
+++ build-index.mjs
@@ -6,5 +6,9 @@ const dirs = (await readdir(".", { withFileTypes: true }))
 	.filter(x => x.isDirectory() && !x.name.startsWith("."))
 	.map(x => x.name);
 
-const html = (await readFile("index.template.html", "utf-8")).replace("{exercises}", dirs.map(x => `\t\t\t<li><a href="${x}">${x}</a></li>`).join("\n"));
+const html = (await readFile("index.template.html", "utf-8")).replace("{exercises}", dirs.map(x => `\t\t\t<li><a href="${x}">${cap(x.replace(/^\d+-/, "").replaceAll("-", " "))}</a></li>`).join("\n"));
 await writeFile("index.html", html);
+
+function cap(s) {
+	return s.charAt(0).toUpperCase() + s.slice(1);
+}
